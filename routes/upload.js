8609a70c3e5604f256759512a1df58c2b946b45a
Ahmed Wheida <ahmed.wheida@gmail.com>
2020-01-07 16:34:07 +0100

    added the router to the app.js

--- routes/upload.js
+++ routes/upload.js
@@ -11,17 +11,23 @@ const path = require('path');
 var rename = Promise.promisify(fs.rename);
 var randomString = Promise.promisify(crypto.randomBytes)
 
-var upload = multer({dest:'uploads/'});
+var upload = multer({
+  dest: 'uploads/'
+});
 var router = express.Router();
 
 //form tag in the front-end must have the following atrribute enctype="multipart/form-data"
 //the input field in the front-end form must be type="file" name="gravetar"
 
-router.post('/', upload.single('gravetar'), function (req, res) {
+router.post('/', upload.single('gravetar'), function(req, res) {
   let uploadedFile = req.file;
   let oldUploadedFileName = uploadedFile.filename;
   let oldUploadedFilePath = uploadedFile.path;
-  let validGravatarExtensions = {".jpg" : 1, ".jpeg" : 1, ".png" : 1}
+  let validGravatarExtensions = {
+    ".jpg": 1,
+    ".jpeg": 1,
+    ".png": 1
+  }
 
 
   if (!uploadedFile || !oldUploadedFileName || !oldUploadedFilePath) {
@@ -39,23 +45,37 @@ router.post('/', upload.single('gravetar'), function (req, res) {
   let gravatar_id
 
 
-  randomString(16).then((str) => {
-    gravatar_id = str;
-    return
-  })
-  .then(() => {
-    return  UsersModel.update({gravatar_id, gravatar_ext}, { where: {User_id} })
-  })
-  .then((result) => {
-    console.log({result});
-    return rename(`${__dirname}/${oldUploadedFileName}${gravatar_ext}`, `${__dirname}/${gravatar_id}${gravatar_ext}`)
-  })
-  .then((data) => {
-      console.log({data})
-  }).catch((err) => {
-    console.log({err})
-    // res.json(err)
-  })
+  randomString(16)
+    .then((str) => {
+      gravatar_id = str;
+      return
+    })
+    .then(() => {
+      return UsersModel.update({
+        gravatar_id,
+        gravatar_ext
+      }, {
+        where: {
+          User_id
+        }
+      })
+    })
+    .then((result) => {
+      console.log({
+        result
+      });
+      return rename(`${__dirname}/${oldUploadedFileName}${gravatar_ext}`, `${__dirname}/${gravatar_id}${gravatar_ext}`)
+    })
+    .then((data) => {
+      console.log({
+        data
+      })
+    }).catch((err) => {
+      console.log({
+        err
+      })
+      // res.json(err)
+    })
 
 });
 
