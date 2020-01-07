292ef0c7f53edda20b212f722ed548d74691aa83
hashem-buzer <hbuzer98@gmail.com>
2020-01-07 21:50:59 +0100

    comments resolved

--- routes/messages.js
+++ routes/messages.js
@@ -1,31 +1,36 @@
 var express = require("express");
 var router = express.Router();
-const Message = require("../models/Messages");
+const MessagesModel = require("../models/Messages");
 const sequelize = require("../models/index");
-
 sequelize.sync().then(data => console.log(data));
 
 router.get("/", (req, res, next) => {
   res.send("respond with a message");
 });
 
-router.post("/", (req, res) => {
+router.post("/store", (req, res) => {
   let body = req.body;
   console.log({ body });
 
-  // let msg = body.messageContent;
-  // Messages.create(msg)
-  //   .then(message => {
-  //     res.json(msg);
-  //   })
-  //   .catch(err => {
-  //     res.json(err);
-  //   });
   let msg = body.messageContent;
-  Message.create({
+  MessagesModel.create({
     messageContent: msg
-  });
+  })
+    .then(message => {
+      res.json(msg);
+    })
+    .catch(err => {
+      res.json(err);
+    });
   res.send(msg);
 });
+// let msg = body.messageContent;
+// MessagesModel.create(msg)
+//   .then(message => {
+//     res.json(msg);
+//   })
+//   .catch(err => {
+//     res.json(err);
+//   });
 
 module.exports = router;
