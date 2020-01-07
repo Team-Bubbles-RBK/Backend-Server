1f68d888105019da7e78fee06366673a94fe7828
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-07 10:03:15 +0100

    basic sequelize opertaion works

---
+++ models/Users.js
@@ -0,0 +1,19 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('./Index');
+
+class User extends Model {
+}
+
+User.init({
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE
+}, {sequelize, modelName: 'user'});
+
+sequelize.sync()
+    .then(() => User.create({
+        username: 'janedoe',
+        birthday: new Date(1980, 6, 20)
+    }))
+    .then(jane => {
+        console.log(jane.toJSON());
+    });
