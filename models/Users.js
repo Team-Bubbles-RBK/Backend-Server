d34b6ba19af2c324e6c1c9c263afc2858321dc6f
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-07 11:01:18 +0100

    added sign-up route and create User model

--- models/Users.js
+++ models/Users.js
@@ -1,19 +1,48 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('./Index');
 
-class User extends Model {
+class Users extends Model {
+    get fullName() {
+        return this.firstname + ' ' + this.lastname;
+    }
+
+    set password(password) {
+        // set hash and salt on user creation
+        const hash = password + '@@@';
+        this.setDataValue('hash', hash);
+    }
+
+    authenticate(username, password) {
+        // hash and compare and return boolean
+    }
 }
 
-User.init({
-    username: DataTypes.STRING,
-    birthday: DataTypes.DATE
-}, {sequelize, modelName: 'user'});
+Users.init(
+    {
+        first_name: {type: DataTypes.STRING, allowNull: false},
+        last_name: {type: DataTypes.STRING, allowNull: false},
+        dob: {type: DataTypes.DATE, allowNull: false},
+        gender: {type: DataTypes.ENUM('Male', 'Female', 'Unknown'), defaultValue: 'Unknown'},
+        username: {type: DataTypes.STRING, allowNull: false},
+        hash: {type: DataTypes.STRING, allowNull: false},
+        salt: {type: DataTypes.STRING, allowNull: false},
+        gravatar_id: {type: DataTypes.STRING, allowNull: false},
+        gravatar_ext: {type: DataTypes.STRING, allowNull: false},
+    }, {
+        sequelize,
+        modelName: 'users',
+        underscored: true,
+    }
+);
 
-sequelize.sync()
-    .then(() => User.create({
-        username: 'janedoe',
-        birthday: new Date(1980, 6, 20)
-    }))
-    .then(jane => {
-        console.log(jane.toJSON());
-    });
+// sequelize.sync()
+//     .then(() => Users.create({
+//         username: 'janedoe',
+//         birthday: new Date(1980, 6, 20)
+//     }))
+//     .then(jane => {
+//         console.log(jane.toJSON());
+//     });
+
+// Export the model in order to use it to query the table
+module.exports = Users;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const UsersModel = require('../models/Users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+/**
+ * POST Route
+ * Signup new user to the system.
+ */
+
+router.post('/sign-up', function (req, res) {
+  res.json("")
+  // UsersModel.create({})
 });
 
 module.exports = router;
