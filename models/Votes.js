703363fa7f7f63531b20c3e8aff54228a6ef2318
hashem-buzer <hbuzer98@gmail.com>
2020-01-08 16:35:43 +0100

    relationships done exept functions

--- models/Votes.js
+++ models/Votes.js
@@ -1,23 +1,35 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("./Index");
+const Bubbles = require("./Bubbles");
+const Users = require("./Users");
 
 class Votes extends Model {
-  get result() {}
+  get result() {
+    if (Votes.findOne({ where: { result: false } })) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 Votes.init(
   {
-    id: { type: DataTypes.INTEGER, AUTO_INCREMENT, primaryKey: true },
-    result: { type: DataTypes.BOOLEAN },
-    invitor_ID: { type: DataTypes.INTEGER },
-    invitee_ID: { type: DataTypes.INTEGER, allowNull: false }
+    result: { type: DataTypes.BOOLEAN }
   },
   {
     sequelize,
-    modelName: "messages",
+    modelName: "Votes",
     underscored: true
   }
 );
 
+Bubbles.hasMany(Votes);
+Votes.belongsTo(Bubbles);
+
+Votes.hasMany(Users);
+Users.belongsTo(Votes, { as: "invitee" }, { as: "invitor" });
+
 // Export the model in order to use it to query the table
+sequelize.sync();
 module.exports = Votes;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("messages", "root", "00000000", {
+const sequelize = new Sequelize("bubble", "root", "", {
   dialect: "mysql"
 });
 
