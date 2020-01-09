7a13f91302a67d74354cf8bbddc2af68367ae98e
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-09 19:51:16 +0100

    removed commented relationships

--- models/Invitations.js
+++ models/Invitations.js
@@ -75,14 +75,8 @@ Invitations.init(
 );
 
 // Define relationships between models
-// Users.hasMany(Votes, {as: 'inviter', foreignKey: 'inviter_id'});
 Votes.belongsTo(Invitations);
-
 Invitations.hasMany(Votes);
-// Invitations.belongsTo(Users, {as: "invitee", foreignKey: "invitee_id"});
-// Invitations.belongsTo(Bubbles);
-
-// Bubbles.hasMany(Invitations);
 
 // Export the model in order to use it to query the table
 sequelize.sync();
