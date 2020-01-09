398886a46a4c40f76b76c11da7577ff502409da3
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-09 12:05:38 +0100

    removed fluff from votes model

--- models/Votes.js
+++ models/Votes.js
@@ -1,29 +1,20 @@
-const { Model, DataTypes } = require("sequelize");
+const {Model, DataTypes} = require("sequelize");
 const sequelize = require("./Index");
-const Bubbles = require("./Bubbles");
-const Users = require("./Users");
 
 class Votes extends Model {
-  get result() {
-    if (Votes.findOne({ where: { result: false } })) {
-      return false;
-    } else {
-      return true;
-    }
-  }
 }
 
 Votes.init(
-  {
-    result: { type: DataTypes.BOOLEAN }
-  },
-  {
-    sequelize,
-    modelName: "Votes",
-    underscored: true
-  }
+    {
+        result: {type: DataTypes.BOOLEAN}
+    },
+    {
+        sequelize,
+        modelName: "Votes",
+        underscored: true
+    }
 );
 
-// Export the model in order to use it to query the table
 sequelize.sync();
+// Export the model in order to use it to query the table
 module.exports = Votes;
