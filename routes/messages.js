e085ebd241dc6435fd69bbc10629b0ad2154d7e1
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-12 20:01:07 +0100

    updated message store and made it require auth

--- routes/messages.js
+++ routes/messages.js
@@ -3,27 +3,30 @@ var router = express.Router();
 const MessagesModel = require("../models/Messages");
 const validators = require('../userInputValidators/validators');
 const io = require('../bin/www');
+const passport = require('passport');
 
 router.get("/", (req, res, next) => {
     res.send("respond with a message");
 });
 
-router.post("/store", validators['messagesStoreValidatorArray'], validators['validatorfunction'], (req, res) => {
-    // Todo fix validation problem
-    let {content, user_id, bubble_id} = req.body;
+router.post("/store", passport.authenticate('jwt', {session: false}), validators['messagesStoreValidatorArray'],
+    validators['validatorfunction'], (req, res) => {
 
-    MessagesModel.store(
-        content,
-        user_id,
-        bubble_id
-    ).then(message => {
-        // Get the IO object and send message to all users in the bubble
-        io.getIO().sockets.to(bubble_id).emit('message', message);
-        res.sendStatus(201);
-    }).catch(err => {
-        console.log({err});
-        res.sendStatus(500);
+        let {content, bubble_id} = req.body;
+        user_id = req.user.id;
+
+        MessagesModel.store(
+            content,
+            user_id,
+            bubble_id
+        ).then(message => {
+            // Get the IO object and send message to all users in the bubble
+            io.getIO().sockets.to(bubble_id).emit('message', message);
+            res.sendStatus(201);
+        }).catch(err => {
+            console.log({err});
+            res.sendStatus(500);
+        });
     });
-});
 
 module.exports = router;
